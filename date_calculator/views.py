e597a971d5765ff5df0f04cffae166f50955e71f
machine-i <wanderson.rene12@gmail.com>
2024-08-26 11:31:53 -0300

    refactoring_in_progress

--- date_calculator/views.py
+++ date_calculator/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 import calendar
+from datetime import datetime
 
-month_names = {'1': 'Jan', '2': 'Fev', '3': 'Mar', '4': 'Abr', '5': 'Mai', '6': 'Jun', '7': 'Jul', '8': 'Ago', '9': 'Set', '10': 'Out', '11': 'Nov', '12': 'Dez'}
+month_names = {'1': 'Jan', '2': 'Fev', '3': 'Mar', '4': 'Abr', '5': 'Mai', '6': 'Jun', '7': 'Jul', '8': 'Ago',
+               '9': 'Set', '10': 'Out', '11': 'Nov', '12': 'Dez'}
 last_days = {0: 31, 1: 28, 2: 31, 3: 30, 4: 31, 5: 30, 6: 31, 7: 31, 8: 30, 9: 31, 10: 30, 11: 31}
 
-def work_days(work_date, day_off):
 
+def work_days(work_date, day_off):
     date = [int(i) for i in work_date]
-    
+
     for m in last_days.keys():
         if m == date[1] - 1:
             date[0] += day_off + 1  # A próxima data de trabalho é 1 dia após o último dia de folga.
@@ -23,23 +25,25 @@ def work_days(work_date, day_off):
 
     return work_date
 
+
 def calculate(request):
     work = request.GET.get('work_day', '')
     off = request.GET.get('day_off', '').strip()
 
     if work == '' or off == '':
-            return redirect('date_calculator:index')
-        
+        return redirect('date_calculator:index')
+
     work = list(reversed(work.split('-')))
     work[1] = str(int(work[1]))
     off = int(off)
-    
+
     print('work', work)
     print('off', off)
 
     list_dates = []
+    current_year = datetime.now().year
 
-    while int(work[2]) == 2023:
+    while int(work[2]) == current_year:
         list_dates.append(work)
         work = work_days(work, off)
 
@@ -52,14 +56,14 @@ def calculate(request):
     cal = calendar.Calendar()
     con = dict()
     for i in months:
-        con[i] = [[], [[], [], [], [], [], []]]
+        con[i] = [[], [[], [], [], [], [], []]]  # the key is the month | con[i][0] -> working days | con[i][1] -> days of the month
         for j in list_dates:
             if month_names[j[1]] != i:
                 continue
             con[i][0].append(int(j[0]))
             con[i].append(j[1]) if j[1] not in con[i] else None
         row = 0
-        for d in cal.itermonthdays(2023, int(con[i][2])):
+        for d in cal.itermonthdays(current_year, int(con[i][2])):
             if len(con[i][1][row]) > 6:
                 row += 1
             con[i][1][row].append(d)
@@ -76,6 +80,7 @@ def calculate(request):
         'date_calculator/index.html',
         context,
     )
+
 
 def index(request):
     return render(
